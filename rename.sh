ac451f0b16482dcc77c5758534bb4652aa417abe
Cody Carvel <cody_carvel@brown.edu>
2021-08-28 12:25:34 -0400

    updates

--- rename.sh
+++
@@ -1,3 +0,0 @@
-for txt in *.md; do
-    { echo '---\ntitle: CIRUELAS MONTESES\nplant-name: 1 CIRUELAS MONTESES\nplant-number: 001\nplant-xml: /assets/xml/plant001.xml\nlayout: single-xml\n'; cat "$md"; } >"$md.tmp" && mv "$md.tmp" "$md"
-done
